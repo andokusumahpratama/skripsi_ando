4f165106dc71a181beaba54f44a906eabc3d22d8
andokusumahpratama <ando.kusumapratama@gmail.com>
2023-05-09 09:38:29 +0700

    Modifikasi route

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [DasboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function(){
 
